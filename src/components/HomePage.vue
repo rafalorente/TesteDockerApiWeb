aaa6500f1b6e7ca0d08b30e5d18753a705a0f0e9
Rafael Lorente <rafaelvieiralorente@gmail.com>
2022-04-28 00:46:25 -0300

    importando componentes para a homepage

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,21 +1,44 @@
 <template>
   <div class="hello">
-   
+    <v-row class="w-100 d-flex">
+   <SelectField @update:value="pais = $event"/>
+   <SendButton @buscar="pesquisarUniversidade();"/>
+   <DataTable v-if="listaUniversidade.length > 0" :lista="listaUniversidade"/>
+   </v-row>
   </div>
 </template>
 
 <script>
-// import axios from 'axios';
+import axios from 'axios';
+import SelectField from './inputs/SelectField.vue'
+import SendButton from './buttons/SendButton.vue'
+import DataTable from './tables/DataTable.vue'
 
 export default {
   name: 'HomePage',
   props: {
     msg: String
+  },components:{
+    SelectField,
+    SendButton,
+    DataTable
+  },
+  data(){
+    return{
+      pais: null,
+      listaUniversidade: []
+    }
   },
   mounted(){
-    // axios
-    //   .get('https://localhost:5001/api/Universidade/GetUniversidadePorPais?pais=brazil')
-    //   .then(response => (console.log(response)));
+  },
+  methods:{
+    pesquisarUniversidade(){
+      axios
+        .get(`https://localhost:49153/api/Universidade/GetUniversidadePorPais?pais=${this.pais.toLowerCase()}`)
+        .then(response => {
+          this.listaUniversidade = response.data
+        });
+    }
   }
 }
 </script>
