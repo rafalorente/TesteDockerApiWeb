d4830b4cf9dae857f3fc24d904b7346b44d2495a
Rafael Lorente <rafaelvieiralorente@gmail.com>
2022-04-27 23:11:09 -0300

    Initial commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+   
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String
+  },
+  mounted(){
+    axios
+      .get('https://localhost:5001/api/Universidade/GetUniversidadePorPais?pais=brazil')
+      .then(response => (console.log(response)));
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
